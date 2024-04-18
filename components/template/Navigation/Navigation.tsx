4b259d2861f7f9b687c81c0f47768d5d0cd8e22a
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-04-18 22:08:41 +0330

    Create Navigation

--- components/template/Navigation/Navigation.tsx
+++ components/template/Navigation/Navigation.tsx
@@ -8,13 +8,13 @@ const Navigation = () => {
     const themeContext = useThemeProvider()
 
     return (
-        <aside className={`flex transition-all absolute backdrop-blur-3xl top-0 w-full ${themeContext?.isOpenSidebar ? "right-0 opacity-100" : "-right-[100%] invisible opacity-0"}`}>
-            <div className='w-80 bg-gray-50 h-screen'>
-                <CloseSidebarButton />
-            </div>
-            <div className="flex-1 h-screen" onClick={() => themeContext?.handleSetIsOpenSidebar(false)}>
-            </div>
-        </aside>
+            <aside className={`flex transition-all absolute backdrop-blur-3xl top-0 w-full ${themeContext?.isOpenSidebar ? "right-0 opacity-100" : "-right-[100%] invisible opacity-0"}`}>
+                <div className='w-80 bg-gray-50 h-screen'>
+                    <CloseSidebarButton />
+                </div>
+                <div className="flex-1 h-screen" onClick={() => themeContext?.handleSetIsOpenSidebar(false)}>
+                </div>
+            </aside>
     )
 }
 
