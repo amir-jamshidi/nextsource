31ee11e7e50728652628bc241f5fd2c01d046097
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-05-01 21:21:13 +0330

    Create Comment Pagination

--- app/(root)/product/[productHref]/page.tsx
+++ app/(root)/product/[productHref]/page.tsx
@@ -25,7 +25,6 @@ const Product = async ({ params: { productHref }, searchParams: { comments } }: 
     const [product, isLoginUser, isHavPlanUser]: [product: IProduct | boolean, isLoginUser: boolean | IUser, isHavPlanUser: boolean | IUser] = await Promise.all([getProductByHref(productHref), isLogin(), isHavPlan()])
     if (!product) return notFound();
     const [accessToSource, isHasToFav]: [accessToSource: boolean, isHasToFav: boolean] = await Promise.all([isAccessToSource(product._id), isHasToFavorite(product._id)]);
-
     const commentPage = comments || '1'
 
     // const addresses = [
