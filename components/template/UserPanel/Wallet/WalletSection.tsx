da28e6fc54a0462952fbe2e1c01256262a95c6e5
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-05-23 23:28:05 +0330

    Create Deposit And Withdraw Server Actions

--- components/template/UserPanel/Wallet/WalletSection.tsx
+++ components/template/UserPanel/Wallet/WalletSection.tsx
@@ -56,6 +56,9 @@ const WalletSection = async () => {
                         <>
                             {deposit.price > 0 && (
                                 <div className='flex justify-between bg-gray-900 p-2 rounded-xl'>
+                                   
+                                   
+                                   
                                     <div className="text-sm flex items-center">
                                         <p className='text-gray-300 border-l border-gray-800 pl-2 ml-2'>{deposit.title}</p>
                                         <p className='text-green-500 mt-1'>{deposit.createdAt.toLocaleDateString('fa-IR')}</p>
@@ -66,7 +69,7 @@ const WalletSection = async () => {
                                         <p className='text-green-500'>تومان</p>
                                     </div>
 
-                                    
+
                                 </div>
                             )}
                         </>
