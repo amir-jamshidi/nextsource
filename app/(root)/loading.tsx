1ad47aebcca2a1448dfc8fb4d21cf4854888d544
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-06-23 15:16:13 +0330

    Create Loading For Home Page

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="donut z-10">
+            </div>
+        </div>
+    )
+}
+
+export default Loading
--- components/shared/BreadCrump.tsx
+++ components/shared/BreadCrump.tsx
@@ -10,7 +10,7 @@ const BreadCrump = ({ addresses }: BreadCrumpProps) => {
     return (
         <div className='flex gap-x-0.5 mt-6 lg:mt-14 items-center text-sm rounded-xl px-4 bg-blue py-3 justify-center'>
             <Link href={'/'} className='bg-gray-900 px-2 py-1 rounded-xl border border-gray-800'>
-                <p className='text-gray-300 text'>صفحه اصلی</p>
+                <p className='text-gray-300 text line-clamp-1'>صفحه اصلی</p>
             </Link>
             {addresses?.map(address => (
                 <div key={address.title} className='flex items-center gap-x-0.5'>
@@ -18,7 +18,7 @@ const BreadCrump = ({ addresses }: BreadCrumpProps) => {
                         <ArrowLeftRounded className='text-gray-400' />
                     </span>
                     <Link href={address.href}>
-                        <p className='text-gray-300 bg-gray-900 rounded-xl px-2 py-1 border border-gray-800'>{address.title}</p>
+                        <p className='text-gray-300 bg-gray-900 rounded-xl px-2 py-1 border border-gray-800 line-clamp-1'>{address.title}</p>
                     </Link>
                 </div>
             ))}
