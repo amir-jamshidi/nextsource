bde07b4fcf490faaa1c338416469f031c252ec43
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-08-27 22:38:30 +0330

    Implement Light Mode

--- components/template/UserPanel/Info/UserInfoSection.tsx
+++ components/template/UserPanel/Info/UserInfoSection.tsx
@@ -40,29 +40,29 @@ const UserInfoSection = ({ user }: { user: IUser }) => {
   return (
     <div>
       <div className='h-12 bg-blue rounded-xl flex-center'>
-        <p className='text-gray-300 text-sm'>شما از تاریخ {new Date(createdAt).toLocaleDateString('fa-IR')} و ساعت  {new Date(createdAt).toLocaleTimeString('fa-IR')} کنار مایی :))</p>
+        <p className='text-700-300 text-sm'>شما از تاریخ {new Date(createdAt).toLocaleDateString('fa-IR')} و ساعت  {new Date(createdAt).toLocaleTimeString('fa-IR')} کنار مایی :))</p>
       </div>
       <div className="flex-center mt-6 gap-x-3 mx-4">
-        <span className='flex-1 h-px bg-gray-800'></span>
-        <p className="text-sm text-gray-300">ویرایش اطلاعات حساب من</p>
-        <span className='flex-1 h-px bg-gray-800'></span>
+        <span className='flex-1 h-px bg-gray-200 dark:bg-gray-800'></span>
+        <p className="text-sm text-600-400">ویرایش اطلاعات حساب من</p>
+        <span className='flex-1 h-px bg-gray-200 dark:bg-gray-800'></span>
       </div>
       <form className="mt-6" onSubmit={handleSubmitForm}>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-1'>
-          <div className='bg-gray-900 rounded-xl border border-gray-800'>
-            <input onChange={(e) => setFullname(e.target.value)} value={fullname} type="text" className='h-12 w-full text-sm bg-gray-900 outline-none border-none text-gray-200 px-2 rounded-xl' placeholder='نام کامل شما' />
+          <div className='bg-input-container rounded-xl border border-gray-800'>
+            <input onChange={(e) => setFullname(e.target.value)} value={fullname} type="text" className='h-12 w-full text-sm bg-input outline-none border-none text-gray-200 px-2 rounded-xl' placeholder='نام کامل شما' />
           </div>
-          <div className='bg-gray-900 rounded-xl border border-gray-800'>
-            <input onChange={(e) => setEmail(e.target.value)} value={email} type="text" className='h-12 w-full text-sm bg-gray-900 outline-none border-none text-gray-200 px-2 rounded-xl' placeholder='ایمیل شما' />
+          <div className='bg-input-container rounded-xl border border-gray-800'>
+            <input onChange={(e) => setEmail(e.target.value)} value={email} type="text" className='h-12 w-full text-sm bg-input outline-none border-none text-gray-200 px-2 rounded-xl' placeholder='ایمیل شما' />
           </div>
-          <div className='bg-gray-900 rounded-xl border border-gray-800'>
-            <input disabled={true} value={phone} type="text" className='h-12 w-full text-sm bg-gray-900 outline-none border-none text-gray-200 px-2 rounded-xl cursor-not-allowed' placeholder='شماره همراه شما' />
+          <div className='bg-input-container rounded-xl border border-gray-800'>
+            <input disabled={true} value={phone} type="text" className='h-12 w-full text-sm bg-input outline-none border-none text-gray-200 px-2 rounded-xl cursor-not-allowed' placeholder='شماره همراه شما' />
           </div>
         </div>
-        <div className='w-full mt-1 rounded-2xl border bg-gray-900 border-gray-800'>
-          <textarea onChange={(e) => setBio(e.target.value)} value={bio} placeholder='بیوگرافی شما' className=' bg-gray-900 border-gray-100 rounded-2xl p-3 border-none outline-none text-gray-200 text-sm min-h-44 max-h-52 w-full'></textarea>
+        <div className='w-full mt-1 rounded-2xl border bg-input-container border-gray-800'>
+          <textarea onChange={(e) => setBio(e.target.value)} value={bio} placeholder='بیوگرافی شما' className=' bg-input border-gray-100 rounded-2xl p-3 border-none outline-none text-gray-200 text-sm min-h-44 max-h-52 w-full'></textarea>
         </div>
-        <input disabled={isLoading} type="submit" className={`h-12 w-full bg-blue mt-1 rounded-xl ${isLoading ? 'text-gray-500' : 'text-gray-200'} cursor-pointer transition-colors`} value={isLoading ? 'لطفا صبر کن ...' : 'ثبت تغییــرات'} />
+        <input disabled={isLoading} type="submit" className={`h-12 w-full bg-btns mt-1 rounded-xl ${isLoading ? 'text-gray-500' : 'text-gray-100 dark:text-gray-200'} cursor-pointer transition-colors`} value={isLoading ? 'لطفا صبر کن ...' : 'ثبت تغییــرات'} />
       </form>
     </div>
   )
