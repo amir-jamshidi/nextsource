b4ead95a2605e533f0d6fa8a6c5083e0d1218efe
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2024-09-20 22:53:30 +0330

    Update BackButton.tsx

--- components/buttons/BackButton/BackButton.tsx
+++ components/buttons/BackButton/BackButton.tsx
@@ -4,7 +4,6 @@ import { useRouter } from 'next/navigation'
 
 const BackButton = ({ bg }: { bg?: boolean }) => {
     const router = useRouter();
-
     return (
         <button onClick={() => router.back()} className={`top-1 left-1 flex justify-center items-center absolute w-10 h-10 rounded-full ${bg ? 'text-gray-300 dark:text-gray-300 bg-items dark:bg-blue' : 'text-gray-600 dark:text-gray-300'} `}>
             <ArrowBackIosRounded fontSize='small' className=" mr-1" />
